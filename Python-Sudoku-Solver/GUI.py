a805becf70a130f788b16bfd2cde5e8c7bee1e23
Chase Staples <s528345@nwmissouri.edu>
2020-11-04 14:19:48 -0600

    Starting on the GUI
    Implementing def function snubbing and importing py game

---
+++ Python-Sudoku-Solver/GUI.py
@@ -0,0 +1,68 @@
+# GUI.py
+import pygame
+import time
+pygame.font.init()
+
+class Grid:
+    board = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
+def __init__(self, rows, cols, width, height, win):
+    pass
+
+def update_model(self):
+    pass
+
+def place(self, val):
+    pass
+
+def sketch(self, val):
+    pass
+
+def draw(self):
+    pass
+
+def select(self, row, col):
+    pass
+
+def clear(self):
+    pass
+
+def click(self, pos):
+    pass
+
+def is_finished(self):
+    pass
+
+def solve(self):
+    pass
+
+def solve_GUI(self):
+    pass
+
+def find_empty(bo):
+    pass
+
+def valid(bo, num, pos):
+    pass
+
+
+def redraw_window(win, board, time, strikes):
+    pass
+
+def format_time(secs):
+    pass
+
+def main():
+    pass
+
+main()
+pygame.quit()
